8e6b61c8d4b5dfe1ac0691186db6b1bfcccc378e
Thrishal18 <116994909+Thrishal18@users.noreply.github.com>
2025-08-12 04:34:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_directions(api_key, origin, destination):
 # --- STREAMLIT APP ---
 
 # Set the title of the web app
-st.title("📍 Google Maps Route Planner")
+st.title("📍 Google Maps Path Viewer")
 
 # Load the API key from environment variables
 api_key = os.getenv("GOOGLE_MAPS_API")
@@ -76,3 +76,4 @@ else:
         else:
 
             st.warning("Please enter correct password and include origin and a destination.")
+
